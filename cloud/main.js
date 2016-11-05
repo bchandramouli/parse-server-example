4a990d66665b4d0cb813059f2482281f779b44b3
bchandramouli <chandramouli.balasubramanian@gmail.com>
2016-11-04 19:28:59 -0700

    added a debug to print inventory

--- cloud/main.js
+++ cloud/main.js
@@ -130,7 +130,7 @@ var Mailgun = require('mailgun-js')({apiKey: "key-afab485a6a9bf921692f83c3c1d03b
 function stringifyHomeInventory(homeInv, price) {
   var orderStringified = "";
   for (var i = 0 ; i < homeInv.length; i++) {
-    console.log("home inve = ", homeInv[i].toString());
+    console.log("home inv = ", homeInv[i].toString());
        orderStringified = orderStringified +
         homeInv[i].get("name") +
         " * " +
