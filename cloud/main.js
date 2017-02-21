dbce0a1854a54a7d7559be5e9bfb6da4fbc9fd8f
bchandramouli <chandramouli.balasubramanian@gmail.com>
2017-02-21 13:29:59 -0800

    consolidate the stripe functionality and iventory - for shared resources

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,4 @@
 // Require all the parse hooks in here and split the defines into separate files
 require('./forage_webhooks.js');
-require('./forage_inventory.js');
 require('./forage_stripe.js');
 require('./forage_insights.js');
